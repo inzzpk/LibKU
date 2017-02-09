c18dfaebbbf04edae8c25d57b6eb149c39d4adfa
Chutiphon.k <chutiphon.k@ku.th>
2017-02-09 22:33:53 +0700

    :alien: Edit link api in test.js

--- src/intro/test.js
+++ src/intro/test.js
@@ -10,14 +10,16 @@ export default class test extends Component {
     state = { albums: [] }
 
     componentWillMount() {
-    //axios.get('https://inzzpk.in.th/book_info.json')
-    axios.get('https://rallycoding.herokuapp.com/api/music_albums')
-      .then(response => this.setState({ albums: response.data }));
+    // axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+    axios.get(`http://158.108.48.254/brequest/book_info.json`)
+      .then(res => this.setState({ albums: res.data }))
+      .catch(err => console.log(err))
+      // .then(response => this.setState({ albums: response.data }));
   }
  
   renderBooks() {
-    return this.state.albums.map(album =>
-      <ListItem>
+    return this.state.albums.map(( album, index ) =>
+      <ListItem key={index}>
           <Thumbnail square size={100} source={require('./img/nov2.jpg')} />
           <Text key={album.title} style={{fontWeight: 'bold'}}>{album.title}</Text>
           <Text note key={album.artist}>{album.artist}</Text>
